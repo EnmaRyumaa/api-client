2e788ed7dae0c0b88a7a075f5a13ff776ae957b6
EnmaRyumaa <fer.augustofs@gmail.com>
2024-12-30 17:13:57 -0300

    Created project

---
+++ src/main/java/com/example/api_client/api_client_ds/ApiClientDsApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_client.api_client_ds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiClientDsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiClientDsApplication.class, args);
+	}
+
+}
