770de4553468cf59f784a5c6efab0eacc0f74086
EnmaRyumaa <fer.augustofs@gmail.com>
2025-01-03 10:31:08 -0300

    findById

---
+++ src/main/java/com/example/api_client/api_client_ds/controllers/ClientController.java
@@ -0,0 +1,29 @@
+package com.example.api_client.api_client_ds.controllers;
+
+import com.example.api_client.api_client_ds.dto.ClientDTO;
+import com.example.api_client.api_client_ds.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/clients")
+public class ClientController {
+
+    @Autowired
+    private ClientService clientService;
+
+    @GetMapping(value = "/{id}")
+    public ClientDTO findById(@PathVariable Long id) {
+        return clientService.findById(id);
+    }
+
+    @GetMapping()
+    public List<ClientDTO> findAll() {
+
+    }
+}
--- src/main/java/com/example/api_client/api_client_ds/services/ClientService.java
+++ src/main/java/com/example/api_client/api_client_ds/services/ClientService.java
@@ -4,7 +4,10 @@ import com.example.api_client.api_client_ds.dto.ClientDTO;
 import com.example.api_client.api_client_ds.entities.Client;
 import com.example.api_client.api_client_ds.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ClientService {
@@ -12,9 +15,16 @@ public class ClientService {
     @Autowired
     private ClientRepository clientRepository;
 
+    @Transactional(readOnly = true)
     public ClientDTO findById(Long id) {
         Client client = clientRepository.findById(id).get();
 
         return new ClientDTO(client);
+    }
+
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        Page<Client> result = clientRepository.findAll(pageable);
+
+        return result.map(x -> new ClientDTO(x));
     }
 }
