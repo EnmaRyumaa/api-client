3563eca2b57240dd65a9264187d5bb0368af54f7
EnmaRyumaa <fer.augustofs@gmail.com>
2025-01-03 09:18:24 -0300

    Created Getter and setter for entity and repository

--- src/main/java/com/example/api_client/api_client_ds/entities/Client.java
+++ src/main/java/com/example/api_client/api_client_ds/entities/Client.java
@@ -3,6 +3,7 @@ package com.example.api_client.api_client_ds.entities;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 @Entity
 public class Client {
@@ -15,5 +16,76 @@ public class Client {
     private LocalDate birthDate;
     private Double income;
     private Integer children;
-   
+
+    public Client() {}
+
+    public Client(Long id, String name, String cpf, LocalDate birthDate, Double income, Integer children) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.birthDate = birthDate;
+        this.income = income;
+        this.children = children;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Double getIncome() {
+        return income;
+    }
+
+    public void setIncome(Double income) {
+        this.income = income;
+    }
+
+    public Integer getChildren() {
+        return children;
+    }
+
+    public void setChildren(Integer children) {
+        this.children = children;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return Objects.equals(id, client.id) && Objects.equals(name, client.name) && Objects.equals(cpf, client.cpf) && Objects.equals(birthDate, client.birthDate) && Objects.equals(income, client.income) && Objects.equals(children, client.children);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, cpf);
+    }
 }
